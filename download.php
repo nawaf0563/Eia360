3bcf2b6e1f0b2b768af02ae06374f037362cef4a
nawaf0563 <78360347+nawaf0563@users.noreply.github.com>
2021-02-01 17:32:11 +0300

    Update download.php

--- download.php
+++ download.php
@@ -7,7 +7,7 @@ $CSVfp = fopen("https://nawaf0563.github.io/Eia360/Analytics-Export.csv", "r");
 if($CSVfp !== FALSE) {
  while(! feof($CSVfp)) {
   $data = fgetcsv($CSVfp, 1000, ",");
-  print_r($data);
+  echo $data;
  }
 }
 fclose($CSVfp);
