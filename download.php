3c71121e86ccda1cd07aa0fae18f6e6c8895e0cc
nawaf0563 <78360347+nawaf0563@users.noreply.github.com>
2021-02-01 17:17:09 +0300

    Update download.php

--- download.php
+++ download.php
@@ -1,35 +1,10 @@
 <?php
-if(isset($_GET['path']))
-{
-//Read the url
-$url = $_GET['path'];
-
-//Clear the cache
-clearstatcache();
-
-//Check the file path exists or not
-if(file_exists($url)) {
-
-//Define header information
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="'.basename($url).'"');
-header('Content-Length: ' . filesize($url));
-header('Pragma: public');
-
-//Clear system output buffer
-flush();
-
-//Read the size of the file
-readfile($url,true);
-
-//Terminate from the script
-die();
+$CSVfp = fopen("https://nawaf0563.github.io/Eia360/Analytics-Export.csv", "r");
+if($CSVfp !== FALSE) {
+ while(! feof($CSVfp)) {
+  $data = fgetcsv($CSVfp, 1000, ",");
+  print_r($data);
+ }
 }
-else{
-echo "File path does not exist.";
-}
-}
-echo "File path is not defined."
-
+fclose($CSVfp);
 ?>
